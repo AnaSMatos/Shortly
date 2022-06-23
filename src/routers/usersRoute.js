6fb25a3c1ac641e24417a3f9c18e1c96aa158686
Luana Lessa <72531277+luanalessa@users.noreply.github.com>
2022-06-22 22:17:26 -0300

    Delete src directory

--- src/routers/usersRoute.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import {getUsers, ranking} from '../controllers/usersController.js';
-
-const usersRouter = Router();
-
-usersRouter.get('/users/:id', getUsers);
-usersRouter.get('/ranking', ranking);
-
-export default usersRouter;
